9f9d7e4b2b62d8f93c7d860e6fdf82a0ab2175da
Lance Salera <lancevincent.salera@cit.edu>
2022-12-16 17:49:52 +0800

    Rename LeapYear components

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -3,7 +3,7 @@ package LeapYear;
 import javax.swing.*;
 
 public class LeapYearGUI {
-    private JPanel panel1;
-    private JTextField textField1;
-    private JButton checkButton;
+    private JPanel pnlMain;
+    private JTextField tfYear;
+    private JButton btnCheckYear;
 }
