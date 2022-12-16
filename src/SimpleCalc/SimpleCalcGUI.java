5a3c7b507be9933b0e83bf9a6b8e9ac0dc932251
Lance Salera <lancevincent.salera@cit.edu>
2022-12-16 18:19:09 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,10 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class SimpleCalcGUI extends JFrame {
     private JPanel pnlMain;
@@ -17,5 +21,47 @@ public class SimpleCalcGUI extends JFrame {
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setTitle("Simple Calculator");
         app.setVisible(true);
+    }
+
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                compute();
+            }
+        });
+
+        tfNumber2.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    compute();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+    }
+
+    void compute() {
+        double num1 = Double.parseDouble(tfNumber1.getText());
+        double num2 = Double.parseDouble(tfNumber2.getText());
+        if(cbOperations.getSelectedItem() == "+") {
+            lblResult.setText(String.format("%.1f", num1+num2));
+        } else if(cbOperations.getSelectedItem() == "-") {
+            lblResult.setText(String.format("%.1f", num1-num2));
+        } else if(cbOperations.getSelectedItem() == "*") {
+            lblResult.setText(String.format("%.1f", num1*num2));
+        } else {
+            lblResult.setText(String.format("%.1f", num1/num2));
+        }
     }
 }
