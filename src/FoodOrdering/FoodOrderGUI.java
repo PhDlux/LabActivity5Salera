b8567dac97492a57df3340ef1e7e0addaee27bd1
Lance Salera <lancevincent.salera@cit.edu>
2022-12-16 18:43:17 +0800

    Complete FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,8 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FoodOrderGUI extends JFrame {
     private JPanel pnlMain;
@@ -23,5 +25,50 @@ public class FoodOrderGUI extends JFrame {
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setTitle("Food Ordering System");
         app.setVisible(true);
+    }
+
+    public FoodOrderGUI() {
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                order();
+            }
+        });
+
+    }
+
+    void order() {
+        int sum = 0;
+        double result = 0;
+        if(cPizza.isSelected()) {
+            sum+=100;
+        }
+        if(cBurger.isSelected()) {
+            sum+=80;
+        }
+        if(cFries.isSelected()) {
+            sum+=65;
+        }
+        if(cSoftDrinks.isSelected()) {
+            sum+=55;
+        }
+        if(cTea.isSelected()) {
+            sum+=50;
+        }
+        if(cSundae.isSelected()) {
+            sum+=40;
+        }
+
+        if(rb5.isSelected()) {
+            result = sum - (sum * 0.05);
+        } else if(rb10.isSelected()) {
+            result = sum - (sum * 0.10);
+        } else if(rb15.isSelected()) {
+            result = sum - (sum * 0.15);
+        } else {
+            result = sum;
+        }
+
+        JOptionPane.showMessageDialog(pnlMain, String.format("The total price is Php %.2f", result));
     }
 }
