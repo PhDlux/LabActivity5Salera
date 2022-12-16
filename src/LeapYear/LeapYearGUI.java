e58af0352c9a4246059a61a22a278586ce7d0e5b
Lance Salera <lancevincent.salera@cit.edu>
2022-12-16 17:57:49 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -3,6 +3,10 @@ package LeapYear;
 import com.sun.nio.sctp.PeerAddressChangeNotification;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class LeapYearGUI extends JFrame {
     private JPanel pnlMain;
@@ -16,5 +20,48 @@ public class LeapYearGUI extends JFrame {
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setTitle("Leap Year Checker");
         app.setVisible(true);
+    }
+
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isLeapYear();
+            }
+        });
+
+        tfYear.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    isLeapYear();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+    }
+
+    void isLeapYear() {
+        int year = Integer.parseInt(tfYear.getText());
+        if(year % 100 == 0) {
+            if(year % 400 == 0) {
+                JOptionPane.showMessageDialog(pnlMain, "Leap year");
+            } else {
+                JOptionPane.showMessageDialog(pnlMain, "Not a leap year");
+            }
+        } else if(year % 4 == 0) {
+            JOptionPane.showMessageDialog(pnlMain, "Leap year");
+        } else {
+            JOptionPane.showMessageDialog(pnlMain, "Not a leap year");
+        }
     }
 }
