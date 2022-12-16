fde0df932b4ecb421bf881c177946e5fa2019d0c
Lance Salera <lancevincent.salera@cit.edu>
2022-12-16 17:53:22 +0800

    Add LeapYear main function

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,9 +1,20 @@
 package LeapYear;
 
+import com.sun.nio.sctp.PeerAddressChangeNotification;
+
 import javax.swing.*;
 
-public class LeapYearGUI {
+public class LeapYearGUI extends JFrame {
     private JPanel pnlMain;
     private JTextField tfYear;
     private JButton btnCheckYear;
+
+    public static void main(String[] args) {
+        LeapYearGUI app = new LeapYearGUI();
+        app.setContentPane(app.pnlMain);
+        app.setSize(500,500);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setTitle("Leap Year Checker");
+        app.setVisible(true);
+    }
 }
